6da539e73874a320600ca60175b23ec6d91f00ce
dk <dk@example.com>
2026-01-20 06:14:26 +0900

    dz 2

--- 02/src/locals.tf
+++ 02/src/locals.tf
@@ -2,6 +2,6 @@ locals {
   lplatform    = "netology-develop-platform"
   ltest         = "test"
   ldb          = "db"
-  vm_test_Uname = "${ local.lplatform }-${ local.lweb }"
+  vm_test_Uname = "${ local.lplatform }-${ local.ltest }"
   vm_db_Uname  = "${ local.lplatform }-${ local.ldb }"
 }
